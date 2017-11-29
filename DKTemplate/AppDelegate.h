fe10dd2345d1cd888322d165aac080a41a33099c
bill <liumin2011@gmail.com>
2017-11-29 09:45:38 +0800

    Initial Commit

---
+++ DKTemplate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DKTemplate
+//
+//  Created by bill on 2017/11/29.
+//  Copyright © 2017年 bill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DKTemplate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DKTemplate
+//
+//  Created by bill on 2017/11/29.
+//  Copyright © 2017年 bill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
